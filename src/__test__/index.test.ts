c1720a1d7f912718181af7ff8a573113cc36a4a4
Sahaja Lal <james@conduit.vc>
2017-12-28 21:58:16 -0700

    Add tests for environment variable fetching

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,6 +1,33 @@
-import subject from '../';
-describe('', () => {
-  it('should behave...', () => {
-    expect(subject).toBe(1);
+import 'reflect-metadata';
+
+import {
+  env,
+  SettingFactory,
+  Settings,
+} from '../';
+
+describe('lifecycle', () => {
+  class OnConfig {}
+  // ts
+  class TestClass extends Settings {
+    @env('_TEST_FOO')
+    public foo: number;
+    @env('_TEST_BAR')
+    public bar: string = 'sup';
+    public config?: OnConfig;
+  }
+
+  it('should allow defaults when environment variable is missing', () => {
+    Object.assign(process.env, {
+      _TEST_FOO: 'foo',
+    });
+
+    const factory = new SettingFactory();
+    const result = factory.create(TestClass);
+    expect(result).toBeInstanceOf(TestClass);
+    expect(result).toMatchObject({
+      foo: 'foo',
+      bar: 'sup',
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,86 @@
-export default 1;
+export interface HandlerType {
+  type: string;
+  propertyKey: string;
+}
+
+export interface EnvDecoratorConfig extends HandlerType {
+  env: string;
+}
+
+export type SuppliedDefaults = {
+  [key: string]: object;
+};
+
+export type AnySettings = {
+  [key: string]: any;
+};
+
+export interface ClassObject<T extends Settings> {
+  new(settings: AnySettings): T;
+}
+
+export function env(name) {
+  const metaKey = Symbol(`env: ${name}`);
+  return (target: any, propertyKey: string) => {
+    const config: EnvDecoratorConfig = {
+      type: 'env',
+      propertyKey,
+      env: name,
+    };
+    Reflect.defineMetadata(metaKey, config, target, 'property');
+  };
+}
+
+export function environmentHandler(
+  value: object,
+  config: EnvDecoratorConfig,
+): string | undefined {
+  const { env: name } = config;
+  return process.env[name];
+}
+
+const ClassValues = Symbol(`class setting values`);
+
+export class SettingFactory {
+  public handlers = {
+    env: environmentHandler,
+  };
+
+  public create<T>(klass: ClassObject<T>, defaults: SuppliedDefaults = {}): T {
+    const keys = Reflect.getMetadataKeys(klass.prototype, 'property');
+    const classMeta = keys.reduce((result, key) => {
+      const meta = Reflect.getMetadata(key, klass.prototype, 'property');
+      const { propertyKey } = meta;
+      const designType = Reflect.getMetadata('design:type', klass.prototype, propertyKey);
+      result.designTypes[propertyKey] = designType;
+
+      const handler = this.handlers[meta.type];
+      if (!handler) {
+        const err =  new Error(`unexpected handler type : ${meta.type}`);
+        throw err;
+      }
+      const value = handler(result.values[propertyKey], meta);
+      if (value === undefined) {
+        return result;
+      }
+      result.values[propertyKey] = value;
+      return result;
+    }, {
+      values: { ...defaults },
+      designTypes: {},
+    });
+
+    return new klass(classMeta.values);
+  }
+}
+
+export class Settings {
+  constructor(values: AnySettings = {}) {
+    for (const key in values) {
+      if (values[key] === undefined) {
+        continue;
+      }
+      this[key] = values[key];
+    }
+  }
+}
